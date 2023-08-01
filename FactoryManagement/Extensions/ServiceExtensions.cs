f56037df87a1d4aeca9df7054123e20785bd3671
Mert Akdemir <comp.eng.m.akdemir@gmail.com>
2023-08-01 15:45:43 +0300

    HttpPut Method For Page Of Update Completed.

--- FactoryManagement/Extensions/ServiceExtensions.cs
+++ FactoryManagement/Extensions/ServiceExtensions.cs
@@ -35,7 +35,9 @@ namespace EmployeeManagement_Sql.Extensions
             services.AddCors(options =>
                 options.AddPolicy(
                     name: "miarmely",
-                    policy => policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5282")
+                    policy => policy
+                        //.WithOrigins("http://127.0.0.1:5500", "http://localhost:5282")
+                        .AllowAnyOrigin()
                         .AllowAnyHeader()
                         .AllowAnyMethod()                    
                         )
